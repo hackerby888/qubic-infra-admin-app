d91c309bdb97d76c3eb9e519bd07dad2715a034f
feiyu <lvp123321123321@gmail.com>
2025-11-13 20:16:20 +0700

    fix overflow terminal ui

--- src/components/common/LocalTerminal.tsx
+++ src/components/common/LocalTerminal.tsx
@@ -1,7 +1,9 @@
 export default function LocalTerminal({ text }: { text: string }) {
     return (
-        <div className="w-full bg-gray-100 p-4 text-sm rounded h-96 overflow-x-scroll">
-            <div className="overflow-auto whitespace-pre-line">{text}</div>
+        <div className="w-full bg-gray-100 p-4 text-sm rounded h-96 overflow-x-auto">
+            <div className="overflow-auto whitespace-pre-line wrap-anywhere">
+                {text}
+            </div>
         </div>
     );
 }
