0750ff9b36a059bc65d2cf55a8d9605ddcb34412
feiyu <lvp123321123321@gmail.com>
2025-11-19 23:45:12 +0700

    env for api

--- src/consts/api-server.ts
+++ src/consts/api-server.ts
@@ -1,1 +1,5 @@
-export const API_SERVER = "http://localhost:3000";
+let _envApiServer = import.meta.env.VITE_API_SERVER;
+if (_envApiServer && _envApiServer.endsWith("/")) {
+    _envApiServer = _envApiServer.slice(0, -1);
+}
+export const API_SERVER = _envApiServer || "http://localhost:3000";
