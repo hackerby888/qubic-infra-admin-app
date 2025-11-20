fed1fd30315db0b26b4e96de6dd528343788b683
feiyu <lvp123321123321@gmail.com>
2025-11-20 15:03:58 +0700

    trigger rebuild

--- src/consts/api-server.ts
+++ src/consts/api-server.ts
@@ -2,4 +2,5 @@ let _envApiServer = import.meta.env.VITE_API_SERVER;
 if (_envApiServer && _envApiServer.endsWith("/")) {
     _envApiServer = _envApiServer.slice(0, -1);
 }
+
 export const API_SERVER = _envApiServer || "http://localhost:3000";
